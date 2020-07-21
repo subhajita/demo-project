b84e36b0bd61a7df8a989458813550b3faff1a09
subhajita <64734015+subhajita@users.noreply.github.com>
2020-07-21 15:53:18 +0530

    number to word

---
+++ day9.js
@@ -0,0 +1,14 @@
+const numberToString=(num)=>
+{
+    const number=["zero","one","two","three","four","five","six","seven","eight","nine"];
+    let string=" ";
+    let numArr=num.toString.split("");
+}
+for(var i=0;i<numArr.length;i++)
+{
+    string +=number[parseInt(numArr[i])]+" ";
+}
+console.log(string);
+numberToString(1234);
+numberToString(90);
+numberToString(117);
